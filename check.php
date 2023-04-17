572e5ad91627b36fed1fe95146db211f949f6587
Tom <64016563+hk59775634@users.noreply.github.com>
2023-04-17 10:11:07 +0800

    Update check.php

--- check.php
+++ check.php
@@ -1,77 +1,50 @@
 <?php
-header('Content-Type: application/json');
-
-function checkBilling($apiKey, $apiUrl) {
-    // 计算起始日期和结束日期
-    $now = new DateTime();
-    $startDate = (new DateTime())->sub(new DateInterval('P90D'));
-    $endDate = (new DateTime())->add(new DateInterval('P1D'));
-    $subDate = (new DateTime())->setDate($now->format('Y'), $now->format('m'), 1);
-
-    // 设置API请求URL和请求头
-    $urlSubscription = "{$apiUrl}/v1/dashboard/billing/subscription"; // 查是否订阅
-    $urlBalance = "{$apiUrl}/dashboard/billing/credit_grants"; // 查普通账单
-    $urlUsage = "{$apiUrl}/v1/dashboard/billing/usage?start_date={$startDate->format('Y-m-d')}&end_date={$endDate->format('Y-m-d')}"; // 查使用量
-    $headers = array(
-        'Authorization: Bearer ' . $apiKey,
-        'Content-Type: application/json'
-    );
-
-    try {
-        // 获取API限额
-        $subscriptionData = json_decode(file_get_contents($urlSubscription, false, stream_context_create(array(
-            'http' => array(
-                'method' => 'GET',
-                'header' => implode("\r\n", $headers)
-            )
-        ))), true);
-        $totalAmount = $subscriptionData['hard_limit_usd'];
-
-        // 判断总用量是否大于20，若大于则更新startDate为subDate
-        if ($totalAmount > 20) {
-            $startDate = $subDate;
-        }
-
-        // 重新生成urlUsage
-        $urlUsage = "{$apiUrl}/v1/dashboard/billing/usage?start_date={$startDate->format('Y-m-d')}&end_date={$endDate->format('Y-m-d')}";
-
-        // 获取已使用量
-        $usageData = json_decode(file_get_contents($urlUsage, false, stream_context_create(array(
-            'http' => array(
-                'method' => 'GET',
-                'header' => implode("\r\n", $headers)
-            )
-        ))), true);
-        $totalUsage = $usageData['total_usage'] / 100;
-
-        // 计算剩余额度
-        $remaining = $totalAmount - $totalUsage;
-
-        // 输出总用量、总额及余额信息
-        return array($totalAmount, $totalUsage, $remaining);
-    } catch (Exception $e) {
-        return array(null, null, null);
-    }
+// 从请求中获取 API KEY
+$key = $_POST['apikey'];
+if (!$key) {
+    http_response_code(400);
+    echo '请提供 API KEY';
+    return;
 }
 
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    $postData = json_decode(file_get_contents('php://input'), true);
-    $apiKey = isset($postData['apikey']) ? trim($postData['apikey']) : '';
+// 计算起始日期和结束日期
+$now = new DateTime();
+$startDate = (new DateTime())->sub(new DateInterval('P90D'));
+$endDate = (new DateTime())->add(new DateInterval('P1D'));
+$subDate = new DateTime();
+$subDate->setDate($now->format('Y'), $now->format('m'), 1);
 
-    if (empty($apiKey)) {
-        http_response_code(400);
-        echo json_encode(array('error' => 'API Key is missing'));
-        exit();
-    }
+// 设置 API 请求 URL 和请求头
+$urlSubscription = 'https://vercel.askopenai.tech/v1/dashboard/billing/subscription';
+$urlUsage = 'https://vercel.askopenai.tech/v1/dashboard/billing/usage?start_date=' . $startDate->format('Y-m-d') . '&end_date=' . $endDate->format('Y-m-d');
+$headers = [
+    'Authorization' => 'Bearer ' . $key,
+    'Content-Type' => 'application/json'
+];
 
-    // 设置默认API链接
-    $apiUrl = 'https://vercel.askopenai.tech';
-    if (isset($postData['apiurl']) && !empty($postData['apiurl'])) {
-        $apiUrl = trim($postData['apiurl']);
-        if (!preg_match('/^https?:\/\//', $apiUrl)) {
-            $apiUrl = 'https://' . $apiUrl;
-        }
-    }
+// 获取 API 限额
+$subscriptionData = json_decode(file_get_contents($urlSubscription, false, stream_context_create(['http' => ['header' => $headers]])));
+if (!$subscriptionData) {
+    http_response_code(401);
+    echo 'API KEY 无效';
+    return;
+}
+$totalAmount = $subscriptionData->hard_limit_usd;
 
-    $data = checkBilling($apiKey, $apiUrl);
-    if (empty($data[0])
+// 判断总用量是否大于20，若大于则更新 startDate 为 subDate
+if ($totalAmount > 20) {
+    $startDate = $subDate;
+    $urlUsage = 'https://vercel.askopenai.tech/v1/dashboard/billing/usage?start_date=' . $startDate->format('Y-m-d') . '&end_date=' . $endDate->format('Y-m-d');
+}
+
+// 获取已使用量
+$usageData = json_decode(file_get_contents($urlUsage, false, stream_context_create(['http' => ['header' => $headers]])));
+$totalUsage = $usageData->total_usage / 100;
+
+// 计算剩余额度
+$remaining = $totalAmount - $totalUsage;
+
+// 输出总用量、总额及余额信息
+echo "Total Amount: " . number_format($totalAmount, 2) . "<br>";
+echo "Used: " . number_format($totalUsage, 2) . "<br>";
+echo "Remaining: " . number_format($remaining, 2) . "<br>";
