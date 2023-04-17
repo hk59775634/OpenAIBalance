0d17f019de1be21a62c6ac16d75bb51a99635c3c
Tom <64016563+hk59775634@users.noreply.github.com>
2023-04-17 10:38:54 +0800

    Update check.php

--- check.php
+++ check.php
@@ -1,50 +1,68 @@
 <?php
-// 从请求中获取 API KEY
-$key = $_POST['apikey'];
-if (!$key) {
+  $request_body = file_get_contents('php://input');
+  $apikey = json_decode($request_body)->apikey;
+
+  if (!$apikey) {
     http_response_code(400);
-    echo '请提供 API KEY';
-    return;
-}
+    echo "请提供 API KEY";
+    exit();
+  }
 
-// 计算起始日期和结束日期
-$now = new DateTime();
-$startDate = (new DateTime())->sub(new DateInterval('P90D'));
-$endDate = (new DateTime())->add(new DateInterval('P1D'));
-$subDate = new DateTime();
-$subDate->setDate($now->format('Y'), $now->format('m'), 1);
+  $apiUrl = "https://api.openai.com";
+  $urlSubscription = "{$apiUrl}/v1/dashboard/billing/subscription";
+  $now = new DateTime();
+  $startDate = new DateTime("-90 days");
+  $endDate = (new DateTime())->add(new DateInterval("P1D"));
+  $subDate = new DateTime();
+  $subDate->setDate($now->format("Y"), $now->format("m"), 1);
 
-// 设置 API 请求 URL 和请求头
-$urlSubscription = 'https://api.openai.com/v1/dashboard/billing/subscription';
-$urlUsage = 'https://api.openai.com/v1/dashboard/billing/usage?start_date=' . $startDate->format('Y-m-d') . '&end_date=' . $endDate->format('Y-m-d');
-$headers = [
-    'Authorization' => 'Bearer ' . $key,
-    'Content-Type' => 'application/json'
-];
+  $headers = [
+    "Authorization" => "Bearer {$apikey}",
+    "Content-Type" => "application/json",
+  ];
 
-// 获取 API 限额
-$subscriptionData = json_decode(file_get_contents($urlSubscription, false, stream_context_create(['http' => ['header' => $headers]])));
-if (!$subscriptionData) {
-    http_response_code(401);
-    echo 'API KEY 无效';
-    return;
-}
-$totalAmount = $subscriptionData->hard_limit_usd;
+  $response = file_get_contents($urlSubscription, false, stream_context_create([
+    "http" => [
+      "method" => "GET",
+      "header" => implode("\r\n", array_map(function($key, $value) {
+        return "{$key}: {$value}";
+      }, array_keys($headers), $headers))
+    ]
+  ]));
 
-// 判断总用量是否大于20，若大于则更新 startDate 为 subDate
-if ($totalAmount > 20) {
+  if (!$response) {
+    http_response_code(400);
+    echo "您的账户已被封禁，请登录OpenAI进行查看。";
+    exit();
+  }
+
+  $subscriptionData = json_decode($response);
+  $totalAmount = $subscriptionData->hard_limit_usd;
+
+  if ($totalAmount > 20) {
     $startDate = $subDate;
-    $urlUsage = 'https://api.openai.com/v1/dashboard/billing/usage?start_date=' . $startDate->format('Y-m-d') . '&end_date=' . $endDate->format('Y-m-d');
-}
+  }
 
-// 获取已使用量
-$usageData = json_decode(file_get_contents($urlUsage, false, stream_context_create(['http' => ['header' => $headers]])));
-$totalUsage = $usageData->total_usage / 100;
+  $urlUsage = "{$apiUrl}/v1/dashboard/billing/usage?start_date={$startDate->format("Y-m-d")}&end_date={$endDate->format("Y-m-d")}";
 
-// 计算剩余额度
-$remaining = $totalAmount - $totalUsage;
+  $response = file_get_contents($urlUsage, false, stream_context_create([
+    "http" => [
+      "method" => "GET",
+      "header" => implode("\r\n", array_map(function($key, $value) {
+        return "{$key}: {$value}";
+      }, array_keys($headers), $headers))
+    ]
+  ]));
 
-// 输出总用量、总额及余额信息
-echo "Total Amount: " . number_format($totalAmount, 2) . "<br>";
-echo "Used: " . number_format($totalUsage, 2) . "<br>";
-echo "Remaining: " . number_format($remaining, 2) . "<br>";
+  $usageData = json_decode($response);
+  $totalUsage = $usageData->total_usage / 100;
+  $remaining = $totalAmount - $totalUsage;
+
+  $response_body = json_encode([
+    "total_amount" => number_format($totalAmount, 2),
+    "total_used" => number_format($totalUsage, 2),
+    "remaining" => number_format($remaining, 2)
+  ]);
+
+  header("Content-Type: application/json");
+  echo $response_body;
